4ae18106947ce26e4df16467043de3fa0e171172
daniel <pedro@ievolutioned.com>
2017-07-13 21:18:26 -0500

    [Added] move cube

--- app/src/main/java/com/tripletres/cardboardprocessing4/sketch/CubeSketch.java
+++ app/src/main/java/com/tripletres/cardboardprocessing4/sketch/CubeSketch.java
@@ -4,23 +4,39 @@ import processing.core.PApplet;
 
 /**
  * Rotating cube example sketch
- *
+ * <p>
  * Created by Daniel on 13/07/2017.
  */
 
 public class CubeSketch extends PApplet {
+
+    private float dx = 300;
+    private float dy = 0;
+
     @Override
     public void settings() {
         fullScreen(STEREO);
+        //size(500, 500);
+    }
+
+    @Override
+    public void setup() {
+
     }
 
     @Override
     public void draw() {
         background(157);
         lights();
-        translate(width/2, height/2);
+        translate(dx, dy);
         rotateX(frameCount * 0.01f);
         rotateY(frameCount * 0.01f);
-        box(500);
+        box(200);
+        //dx++;
+        dy++;
+        if (dx > 1000)
+            dx = 0;
+        if (dy > 2000)
+            dy = 0;
     }
 }
